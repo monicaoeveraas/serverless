8c6810f7b79a497a0ce9afff5e6b33666f8c1229
monicaoeveraas <81336133+monicaoeveraas@users.noreply.github.com>
2021-04-30 15:39:43 -0400

    Update index.js

--- md5sum/index.js
+++ md5sum/index.js
@@ -11,9 +11,9 @@ exports.handler = (event, handler, callback) => {
     // md5sum requires stdin, so this echos and pipes the value to md5sum
     // added sed to remove the trailing " -" that md5sum adds to output
     // added -n because echo includes a newline, causing md5sum to be wrong
-value = "teststring"
 
-
+    var input = event.queryStringParameters.input;
+ 
 
 if (!/^[a-z0-9 .-]*$/i.test(value)) {
 
